4aa377c32f7bf05ec74e217f13853d6bb3bc1054
shangwei chen <a02010704020@gmail.com>
2019-07-17 17:28:23 +0800

    Java and Kotlin variavle data type

--- src/main/java/com/andy/Hello.java
+++ src/main/java/com/andy/Hello.java
@@ -4,7 +4,20 @@ public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
 //        new Person().hello();
+        //物件 = 參照資料型態 通常宣告名稱開頭是大寫
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19; //代表放在物件當中
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        Byte b2 = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "andy";
+
     }
 }
--- src/main/kotlin/com/andy/Hello.kt
+++ src/main/kotlin/com/andy/Hello.kt
@@ -2,9 +2,16 @@ package com.andy
 
 fun main() {
 //    println("Hello kotlin")
-    Human().hello()
+//    Human().hello()
     val h = Human() //不能再改變的變數
     h.hello()
+    var age2 = 19; //變數
+    age2 = age2.plus(other = 1); //增加 1
+    val age = 19;  //不可改變的值
+    var weight = 66.5;
+    var name: String;
+    name = "Andy"
+    print(age2)
 }
 
 class Human {
