5e7879a6b5b73fd9344862d35988416e07819ff6
shangwei chen <a02010704020@gmail.com>
2019-07-18 16:21:49 +0800

    建構子教學

--- src/main/java/com/andy/Hello.java
+++ src/main/java/com/andy/Hello.java
@@ -5,19 +5,24 @@ public class Hello {
 //        System.out.println("Hello world");
 //        new Person().hello();
         //物件 = 參照資料型態 通常宣告名稱開頭是大寫
-        Person p = new Person();
-        p.hello();
-        int age = 19;
-        Integer age2 = 19; //代表放在物件當中
-        char c = '我';
-        Character c2 = 'A';
-        byte b = 120;
-        Byte b2 = 120;
-        float weight = 66.5f;
-        double height = 1.7;
-        boolean adult = true;
-        boolean enroll = false;
-        String name = "andy";
-
+        Person p = new Person("Andy",66.5f,1.7f);
+//        p.hello();
+        p.weight = 70.5f;
+        p.height = 1.9f;
+        System.out.println(p.bmi());
+//        int age = 19;
+//        Integer age2 = 19; //代表放在物件當中
+//        char c = '我';
+//        Character c2 = 'A';
+//        byte b = 120;
+//        Byte b2 = 120;
+//        float weight = 66.5f;
+//        double height = 1.7;
+//        boolean adult = true;
+//        boolean enroll = false;
+//        String name = "andy";
+//        String s = new String("ABCDE");
+//        System.out.println(enroll);
+//        System.out.println(s);
     }
 }
